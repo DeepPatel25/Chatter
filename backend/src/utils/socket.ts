a32f052b3e7d69db2f49c677f76141468c4cb91f
Deep Patel <dp3676991@gmail.com>
2026-01-19 19:55:42 +0530

    fix: remove unused import of error from console in socket utility

--- backend/src/utils/socket.ts
+++ backend/src/utils/socket.ts
@@ -1,7 +1,6 @@
 import { Socket, Server as SocketServer } from "socket.io";
 import { Server as HttpServer } from "http";
 import { verifyToken } from "@clerk/express";
-import { error } from "console";
 import User from "../models/User";
 import Chat from "../models/Chat";
 import Message from "../models/Message";
